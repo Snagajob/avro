eec31cead38a4ac86351f208110ac1a298eeac68
jgonyo-snag <jgonyo@snagajob.com>
2021-02-10 11:46:24 -0500

    support nullable Enum

--- lang/csharp/src/apache/main/Reflect/ClassCache.cs
+++ lang/csharp/src/apache/main/Reflect/ClassCache.cs
@@ -277,10 +277,20 @@ namespace Avro.Reflect
                     LoadClassCache(objType.GenericTypeArguments[1], ms.ValueSchema);
                     break;
                 case NamedSchema ns:
-                    EnumCache.AddEnumNameMapItem(ns, objType);
+                    // add the non-nullable type to the the enum map
+                    if (objType.IsGenericType && objType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    {
+                        EnumCache.AddEnumNameMapItem(ns, objType.GenericTypeArguments[0]);
+                    }
+                    else
+                    {
+                        EnumCache.AddEnumNameMapItem(ns, objType);
+                    }
                     break;
                 case UnionSchema us:
-                    if (us.Schemas.Count == 2 && (us.Schemas[0].Tag == Schema.Type.Null || us.Schemas[1].Tag == Schema.Type.Null) && objType.IsClass)
+                    if (us.Schemas.Count == 2 &&
+                        (us.Schemas[0].Tag == Schema.Type.Null || us.Schemas[1].Tag == Schema.Type.Null) &&
+                        (objType.IsClass || (objType.IsGenericType && objType.GetGenericTypeDefinition() == typeof(Nullable<>))))
                     {
                         // in this case objType will match the non null type in the union
                         foreach (var o in us.Schemas)
